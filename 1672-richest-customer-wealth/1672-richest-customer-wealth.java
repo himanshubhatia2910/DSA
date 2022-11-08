8b4549812c78f593e98ec830704d79ed95f747ed
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2022-11-08 21:32:35 +0530

    Time: 3 ms (23.24%), Space: 43 MB (65.72%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum=0,max=0;
+        for(int i=0;i<accounts.length;i++)
+        {
+            for(int j=0;j<accounts[i].length;j++)
+            {
+                sum = sum+ accounts[i][j];
+                if(max<sum){
+                    max=sum;
+                }
+            }
+            sum=0;
+        }
+        return max; 
+    }
+}

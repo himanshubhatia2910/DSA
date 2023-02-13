181f3af8a74944f54f71579041d49ed1359f13c2
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2023-02-13 22:51:09 +0530

    Added solution - LeetHub

--- Remove duplicate elements from sorted Array - GFG/remove-duplicate-elements-from-sorted-array.java
+++ Remove duplicate elements from sorted Array - GFG/remove-duplicate-elements-from-sorted-array.java
@@ -32,15 +32,15 @@ public class GFG {
 
 class Solution {
     int remove_duplicate(int A[],int N){
-        int i=0;
-        for(int j=1;j<N;j++)
-        {
-            if(A[i]!=A[j])
-            {
-                i++;
-                A[i]=A[j];
-            }
-        }
-        return i+1;
+       int i=0;
+       for(int j=0;j<N;j++)
+       {
+           if(A[i]!=A[j])
+           {
+               i++;
+               A[i]=A[j];
+           }
+       }
+       return i+1;
     }
 }

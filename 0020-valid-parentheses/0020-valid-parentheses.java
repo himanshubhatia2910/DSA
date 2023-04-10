d07f27bb2d5bc3c9e1fedc99a8f882de012bdd64
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2023-04-10 22:47:56 +0530

    Time: 2 ms (79.07%), Space: 40.4 MB (74.90%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<Character>();
+        for(char c:s.toCharArray())
+        {
+            if(c=='(' ||c=='{' ||c=='[')
+            {
+                stack.push(c);
+            }
+            else
+            {
+                if(stack.empty()){return false;}
+                if(c==')' && stack.peek()=='(')
+                {
+                    stack.pop();
+                }
+                else if(c=='}' && stack.peek()=='{')
+                {
+                    stack.pop();
+                }
+                else if(c==']' && stack.peek()=='[')
+                {
+                    stack.pop();
+                }
+                else{return false;}
+            }
+        }
+        return stack.empty();
+    }
+}

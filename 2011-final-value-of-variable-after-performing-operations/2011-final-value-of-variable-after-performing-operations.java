48af636f9b8d39176d524e43a6a841e1e14f89da
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2023-06-01 12:49:01 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (92.83%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+      int x=0;
+      for(String str:operations)
+      {
+          x+= str.charAt(1)=='+'? 1 : -1;
+          
+      }
+        return x;
+    }
+}

a2d17c77e48421a0cc21a76ebcffa4c06da01d95
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2023-07-24 21:27:04 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (20.14%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,28 @@
+class Solution {
+   public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        
+        return powHelper(x, n);
+    }
+    
+    private double powHelper(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        
+        double temp = powHelper(x, n / 2);
+        
+        if (n % 2 == 0) {
+            return temp * temp;
+        } else {
+            return x * temp * temp;
+        }
+    }
+}

330233a3d4e3c8d58d335099cca4a42075870fb5
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2023-08-01 12:57:26 +0530

    Added solution - LeetHub

--- Check if string is rotated by two places - GFG/check-if-string-is-rotated-by-two-places.java
+++ Check if string is rotated by two places - GFG/check-if-string-is-rotated-by-two-places.java
@@ -33,30 +33,31 @@ class Solution
     //another string by exactly 2 places.
     public static boolean isRotated(String str1, String str2)
     {
-       int n = str1.length();
-       int m = str2.length();
-       if(n!=m) return false;
-       
-       boolean clockwise=true;
-       boolean anticlockwise = true;
-       for(int i=0;i<n;i++)
-       {
-           if(str1.charAt(i)!= str2.charAt((i+2) %n))
-           {
-               clockwise=false;
-               break;
-           }
-       }
+        int n = str1.length();
+        int m = str2.length();
+        
+        if(n!=m) return false;
+        
+        boolean clockwise=true;
+        boolean anticlockwise=true;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(str1.charAt(i)!=str2.charAt((i+2)%n))
+            {
+                clockwise=false;
+                break;
+            }
+        }
         for(int i=0;i<m;i++)
-       {
-           if(str1.charAt((i+2)%n)!= str2.charAt(i))
-           {
-               anticlockwise=false;
-               break;
-           }
-       }
-       return (clockwise || anticlockwise);
-       
+        {
+            if(str1.charAt((i+2)%m)!=str2.charAt(i))
+            {
+                anticlockwise=false;
+                break;
+            }
+        }
+        return clockwise || anticlockwise;
     }
     
 }

e6115e635f04aab5aa713bd9e626f65e2fb74aea
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2023-08-01 13:10:45 +0530

    Added solution - LeetHub

--- Nth node from end of linked list - GFG/nth-node-from-end-of-linked-list.java
+++ Nth node from end of linked list - GFG/nth-node-from-end-of-linked-list.java
@@ -100,21 +100,21 @@ class Solution
     //Function to find the data of nth node from the end of a linked list.
     int getNthFromLast(Node head, int n)
     {
-        if(head==null || n<=0)
-        {
-            return -1;
-        }
-        Node slow= head;
-        Node fast = head;
-        for(int i=0;i<n;i++)
-        {
-            if(fast==null) return -1;
-            fast=fast.next;
-        }
-         while (fast != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        return slow.data;
+       if(head==null || n<=0) return -1;
+       
+       Node slow = head;
+       Node fast = head;
+       
+       for(int i=0;i<n;i++)
+       {
+           if(fast==null) return -1;
+           fast=fast.next;
+       }
+       while(fast!=null)
+       {
+           slow = slow.next;
+           fast = fast.next;
+       }
+       return slow.data;
     }
 }

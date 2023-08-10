f20616193cbfe725523ea3b47f90b0862a2056c1
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2023-08-10 10:21:04 +0530

    Time: 3 ms (93.13%), Space: 43.6 MB (48.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,19 +3,19 @@ class Solution {
     static int NO_OF_CHARS=256;
     public boolean isAnagram(String s, String t) {
         int[] count = new int[NO_OF_CHARS];
-        
         if(s.length()!=t.length())
         {
             return false;
         }
+        
         char[] str = s.toCharArray();
         char[] str1 = t.toCharArray();
+        
         for(int i=0;i<s.length();i++)
         {
             count[str[i]]++;
             count[str1[i]]--;
         }
-        
         for(int j=0;j<NO_OF_CHARS;j++)
         {
             if(count[j]!=0) return false;
